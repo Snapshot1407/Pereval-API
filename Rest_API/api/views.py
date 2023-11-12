7d530b64396cc6015c10b22733fbc77e5734bee4
Snap <sailersnap@mail.ru>
2023-11-12 16:43:37 +0600

    Добавление новых методов для REST API

--- Rest_API/api/views.py
+++ Rest_API/api/views.py
@@ -1,5 +1,6 @@
+from rest_framework.response import Response
 from django.http import JsonResponse
-from rest_framework import generics
+from rest_framework import generics, viewsets, mixins
 from .serializers import *
 
 class PerevalAddAPI(generics.CreateAPIView):
@@ -22,3 +23,48 @@ class PerevalAddAPI(generics.CreateAPIView):
             responseData = {'status': '400', 'message': f'Bad Request: {exc}', 'id': 'null'}
             return JsonResponse(responseData, status=400, safe=False)
 
+class PerevalDetailAPI(mixins.RetrieveModelMixin, mixins.UpdateModelMixin, viewsets.GenericViewSet):
+    """Класс работы с БД для второго спринта: извлечение и редактирование перевала"""
+    queryset = PerevalAdd.objects.all()
+    serializer_class = PerevalDetailSerializer
+
+    def update(self, request, *args, **kwargs):
+        """
+        Переопределение метода update(PATCH)
+        """
+        pk = kwargs.get("pk", None)
+
+        try:
+            instance = PerevalAdd.objects.get(pk=pk)
+        except:
+            return Response({"error": "Такого перевала не существует"}, status=400)
+
+        if instance.status != "N":
+            return Response({"message": "Перевал на модерации, вы не можете его изменить",
+                             "state": 0}, status=400)
+        else:
+            serializer = PerevalDetailSerializer(data=request.data, instance=instance)
+            serializer.is_valid(raise_exception=True)
+            serializer.save()
+            return Response({"state": 1}, status=200)
+
+
+class AuthEmailPerevalAPI(mixins.ListModelMixin, viewsets.GenericViewSet):
+    """Класс работы с БД для второго спринта: вывод всех записей по email"""
+    queryset = PerevalAdd.objects.all()
+    serializer_class = AuthEmailPerevalSerializer
+
+    def get(self, request, *args, **kwargs):
+        """
+        Переопределение метода GET для вывода всех записей по email
+        :param request: Json для полей модели перевала (PerevalAdd)
+        :return: Response пример: { "status": 200, "message": null, "id": 42 }
+        """
+        email = kwargs.get('email', None)
+        if PerevalAdd.objects.filter(user__email=email).is_exist == True:
+            responseData = AuthEmailPerevalSerializer(PerevalAdd.objects.filter(user__email=email), many=True).data
+
+        else:
+            responseData = {'message': f'Нет записей от email = {email}'}
+
+        return Response(responseData, status=200)
