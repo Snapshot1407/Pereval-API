d30733699e944fe82b7f07e24d8aa090b435bad5
Snap <sailersnap@mail.ru>
2023-11-12 16:50:59 +0600

    Добавление новых методов(Vievs) для REST API, добавление urls

--- Rest_API/api/urls.py
+++ Rest_API/api/urls.py
@@ -4,4 +4,7 @@ from .views import *
 urlpatterns = [
     # Спринт №1
     path('submitData', PerevalAddAPI.as_view()),
+    # Спринт №2
+    path('submitData/<int:pk>/', PerevalDetailAPI.as_view({'get': 'retrieve', 'patch': 'partial_update'})),
+    path('submitData/user__email=<str:email>', AuthEmailPerevalAPI.as_view({'get': 'list'})),
     ]
